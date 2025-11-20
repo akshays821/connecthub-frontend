61862f85247f9fd84dbd80a39be6fc3a12babc36
akshays821 <akshayshaji821@gmail.com>
2025-11-20 11:08:36 +0530

    replaced alerts with toast

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { initializeSocket, disconnectSocket } from './socket';
 import { addPost } from './Redux/Slices/postSlice.js';
 import { incrementMessageCount } from './Redux/Slices/messageSlice.js';
 import { setNotificationCount } from './Redux/Slices/notificationSlice.js';
+import { Toaster } from 'react-hot-toast';
 
 import Register from './Pages/Register';
 import Login from './Pages/Login';
@@ -47,20 +48,29 @@ function App() {
   }, [isAuthenticated, user]);
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={isAuthenticated ? <Navigate to="/home" /> : <Navigate to="/login" />} />
-        <Route path="/login" element={isAuthenticated ? <Navigate to="/home" /> : <Login />} />
-        <Route path="/register" element={isAuthenticated ? <Navigate to="/home" /> : <Register />} />
-        <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
-        <Route path="/profile/:username" element={isAuthenticated ? <Profile /> : <Navigate to="/login" />} />
-        <Route path="/post/:postId" element={isAuthenticated ? <PostDetail /> : <Navigate to="/login" />} />
-        <Route path="/notifications" element={isAuthenticated ? <Notifications /> : <Navigate to="/login" />} />
-        <Route path="/follow-requests" element={isAuthenticated ? <FollowRequests /> : <Navigate to="/login" />} />
-        <Route path="/messages" element={isAuthenticated ? <Messages /> : <Navigate to="/login" />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 3000,
+        }}
+      />
+      <Router>
+        <Routes>
+          <Route path="/" element={isAuthenticated ? <Navigate to="/home" /> : <Navigate to="/login" />} />
+          <Route path="/login" element={isAuthenticated ? <Navigate to="/home" /> : <Login />} />
+          <Route path="/register" element={isAuthenticated ? <Navigate to="/home" /> : <Register />} />
+          <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
+          <Route path="/profile/:username" element={isAuthenticated ? <Profile /> : <Navigate to="/login" />} />
+          <Route path="/post/:postId" element={isAuthenticated ? <PostDetail /> : <Navigate to="/login" />} />
+          <Route path="/notifications" element={isAuthenticated ? <Notifications /> : <Navigate to="/login" />} />
+          <Route path="/follow-requests" element={isAuthenticated ? <FollowRequests /> : <Navigate to="/login" />} />
+          <Route path="/messages" element={isAuthenticated ? <Messages /> : <Navigate to="/login" />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
